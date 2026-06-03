e4b396dc4bdfc48aa8cfcea756cf3392c166e78d
97694732@qq.com <Long6166@@>
2026-06-03 17:12:19 +0800

    userArea动态化: 存储/读取/可编辑, 不再硬编码13003000000

--- utils/kongfz.js
+++ utils/kongfz.js
@@ -190,13 +190,13 @@ export function fetchItems(token, params = {}, onProgress) {
  * list中每项: {id, title, author, press, priceText, imgBigUrl, shopName, qualityText, pubDateText, postage}
  */
 export function searchProducts(keyword, options = {}) {
-	const { phpsessid = '', page = 1, sortType = '' } = options
+	const { phpsessid = '', page = 1, sortType = '', userArea = '13003000000' } = options
 	return new Promise((resolve, reject) => {
 		const reqData = {
 			dataType: 0,
 			keyword: keyword,
 			page: page,
-			userArea: '13003000000'
+			userArea: userArea
 		}
 		if (sortType) {
 			reqData.sortType = sortType
