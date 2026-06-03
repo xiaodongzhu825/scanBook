bc4f43756ca392f7f782dcd9df8d7a0f00848e98
97694732@qq.com <Long6166@@>
2026-06-03 16:06:41 +0800

    Revert "修复孔夫子搜索：传入PHPSESSID+响应字段映射使用zhizhu正确字段名(title/priceText/shopName等)"
    This reverts commit d9b50574aa0e7e3e6a75799a630188b7a0fde4b7.

--- pages/upload/upload.vue
+++ pages/upload/upload.vue
@@ -934,7 +934,7 @@ export default {
 			})
 
 			// 2. 搜索孔夫子 - 获取在售商品信息
-			searchProducts(this.isbn, { phpsessid: this.kongfzToken || '' }).then(data => {
+			searchProducts(this.isbn).then(data => {
 				this.isLoading = false
 				if (data && data.total > 0) {
 					// 市场统计
@@ -947,16 +947,16 @@ export default {
 					// 在售商品列表（最多12条）
 					const list = (data.list || []).slice(0, 12)
 					this.productList = list.map(item => ({
-						image: item.imgBigUrl || '',
-						totalPrice: item.priceText || '',
+						image: '',
+						totalPrice: item.prodNum + '本在售',
 						bookPrice: '',
-						shippingFee: item.postage && item.postage.shippingList && item.postage.shippingList.length > 0 ? item.postage.shippingList[0].shippingFee : '',
-						condition: item.qualityText || '',
-						shopName: item.shopName || '',
-						bookName: item.title || '',
+						shippingFee: '',
+						condition: item.binding || '',
+						shopName: item.press || '',
+						bookName: item.bookName || '',
 						author: item.author || '',
-						pubDate: item.pubDateText || '',
-						bookId: item.id || ''
+						pubDate: item.pubDate || '',
+						bookId: item.bookId || ''
 					}))
 				} else {
 					this.marketData = { onSale: 0, old: 0, new: 0, sold: 0 }
